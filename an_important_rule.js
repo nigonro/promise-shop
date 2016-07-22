d6d6a7f99847151ab26e1d904eda81cc8d0e982f
Nicolas Gonzalez Rodriguez <nigonro@rrz.cc>
2016-07-22 16:29:26 +0200

    An important rule

---
+++ an_important_rule.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function alwaysThrows() {
+  throw new Error('OH NOES');
+}
+
+function iterate(i) {
+  console.log(i);
+  return ++i;
+}
+
+Promise.resolve(iterate(1))
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(alwaysThrows)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(null, alwaysThrows)
+  .catch(console.log);
